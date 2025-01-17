4a0ba3e7cdd451aff2b3be5101394eee8038aab8
Davi Job <davi.job@hotmail.com>
2025-01-17 10:46:11 -0300

    refactor layout structure and improve meta tags in index.html; add controls and display components

---
+++ src/interface/layout/controls.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function controls() {
+    return (
+        <form className="controls_section">
+            <fieldset>
+                <legend>Control Panel</legend>
+            </fieldset>
+        </form>
+    );
+}
+
+export default controls;
---
+++ src/interface/layout/display.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function display() {
+    return <section className="display_section">display</section>;
+}
+
+export default display;
